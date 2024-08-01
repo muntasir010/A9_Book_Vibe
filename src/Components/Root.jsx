128823f0c5a1d13627236c29bd87c78b7185eebe
Naeem Muntasir <naeemtasir03@gmail.com>
2024-08-01 21:49:02 +0600

    footer section add

--- src/Components/Root.jsx
+++ src/Components/Root.jsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Nav from "./Navbar";
+import Footer from "./Footer";
 
 const Root = () => {
     return (
-        <div className="max-w-7xl mx-5 md:mx-10 sm:mx-10 lg:mx-auto">
-            <Nav/>
-            <Outlet/>
+        <div>
+            <div className="max-w-7xl mx-5 md:mx-10 sm:mx-10 lg:mx-auto">
+                <Nav />
+                <Outlet />
+            </div>
+            <Footer />
         </div>
     );
 };
