98187ce243d53d8cc971e44449eded524a1e6af8
Naeem Muntasir <naeemtasir03@gmail.com>
2024-08-01 15:16:33 +0600

    home card complete

--- src/Components/Book.jsx
+++ src/Components/Book.jsx
@@ -1,9 +1,30 @@
 import PropTypes from 'prop-types';
+import { FaStar } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
-const Book = ({book}) => {
+const Book = ({ book }) => {
+    const { image, bookName, author, rating, category, tags } = book;
     return (
-        <div>
-            <h1>{book.bookName}</h1>
+        <div className="card border-2 p-4">
+            <figure className='bg-base-200 rounded-xl'>
+                <img className='h-56 p-6'
+                    src={image}
+                    alt="Books" />
+            </figure>
+            <div className="card-body">
+                <div className='flex justify-between'>
+                    <a href='/' className=' border-2 px-5 py-2 text-green-400 rounded-full'>{tags[0]}</a>
+                    <a href='/' className='border-2 px-5 py-2 text-green-400 rounded-full'>{tags[1]}</a>
+                </div>
+                <h2 className="text-2xl font-semibold">{bookName}</h2>
+                <p className='font-medium text-gray-500'>By: {author}</p>
+                <p className=' decoration-dashed w-full'></p>
+                <hr className='border-t-2 border-dashed my-3'/>
+                <div className="card-actions justify-between">
+                    <h4 className='font-medium text-gray-500'>{category}</h4>
+                    <h4 className='font-medium text-gray-500 flex gap-3 items-center'>{rating} <span><FaStar/></span></h4>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Books.jsx
+++ src/Components/Books.jsx
@@ -10,9 +10,9 @@ const Books = () => {
         .then(data => setBooks(data))
     },[])
     return (
-        <div>
-            <h3 className="text-4xl font-semibold text-center">Books</h3>
-            <div>
+        <div className="mb-10">
+            <h3 className="text-4xl font-semibold text-center my-10">Books</h3>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {
                     books.map(book => <Book key={book.id} book={book}/>)
                 }
