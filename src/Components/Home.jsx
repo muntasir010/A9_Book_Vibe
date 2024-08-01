47afc78c816cdd2ec863c181f15e49f583cb579d
Naeem Muntasir <naeemtasir03@gmail.com>
2024-08-01 12:31:14 +0600

    Banner section complete

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,13 @@
-// import Banner from "./Banner";
-// import Books from "./Books";
+import Banner from "./Banner";
+import Books from "./Books";
 
-// const Home = () => {
-//     return (
-//         <div>
-//             <Banner/>
-//             <Books/>
-//         </div>
-//     );
-// };
+const Home = () => {
+    return (
+        <div>
+            <Banner/>
+            <Books/>
+        </div>
+    );
+};
 
-// export default Home;
+export default Home;
